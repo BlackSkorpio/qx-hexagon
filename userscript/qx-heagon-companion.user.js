72dc90b93c381f16d57b44e04cc57e52d91b5aa1
Bjarne Varöystrand <black.skorpio+github@gmail.com>
2018-11-05 01:08:52 +0100

    Adding header img for the homepage position section

--- userscript/qx-heagon-companion.user.js
+++ userscript/qx-heagon-companion.user.js
@@ -442,6 +442,7 @@ onReady(function() {/* TODO Remove #whole when beta period is over */
 		var hq_clubHidden		= $('#column_center .list [id^="divx"] .small a[onclick^="openEdit"][style*="bold"]');
 		var hq_clubHeader		= $('#column_center .header [href^="/showclubs.php"]:not([href$="&xxx=1"])');
 		var hq_clubHeaderX		= $('#column_center .header [href^="/showclubs.php"][href$="&xxx=1"]');
+		var hq_MemPosition		= $('.homepageblock.'+hq_ClassPrefix+'member-position .line .header');
 		var hq_QruiserLogoSVG	= hq_svgFragment+'-logo" alt="Qruiser - The Nordic Gay Lesbian & Queer Online Community'+hq_svgFragmentUse+'#qruiser-logo'+hq_svgFragmentSuffix;
 		var hq_qxLogoSVG		= hq_svgFragment+'-qx-logo'+hq_svgFragmentUse+'#qx-logo'+hq_svgFragmentSuffix;
 		var hq_logoutSVG		= hq_svgFragment+'-logout'+hq_svgFragmentUse+'#action-logout'+hq_svgFragmentSuffix;
@@ -455,6 +456,7 @@ onReady(function() {/* TODO Remove #whole when beta period is over */
 		var hq_mailAnsweredSVG	= hq_svgFragment+'-answered'+hq_svgFragmentUse+'#messages-answered'+hq_svgFragmentSuffix;
 		var hq_mailAttachmentSVG = hq_svgFragment+'-attachment'+hq_svgFragmentUse+'#messages-attachment'+hq_svgFragmentSuffix;
 		var hq_padLockSVG		= hq_svgFragment+'-padlock'+hq_svgFragmentUse+'#member-padlock'+hq_svgFragmentSuffix;
+		var hq_MemPositionSVG 	= hq_svgFragment+'-position '+hq_ClassPrefix+'header'+hq_svgFragmentUse+'#member-position'+hq_svgFragmentSuffix;
 		var hq_clearSVG			= hq_svgFragment+'-clear'+hq_svgFragmentUse+'#action-zero'+hq_svgFragmentSuffix;
 		var hq_abuseSVG			= hq_svgFragment+'-abuse'+hq_svgFragmentUse+'#action-abuse'+hq_svgFragmentSuffix;
 		var hq_heartIconSVG		= hq_svgFragment+'-favorite'+hq_svgFragmentUse+'#nav-favorites'+hq_svgFragmentSuffix;
@@ -541,6 +543,7 @@ onReady(function() {/* TODO Remove #whole when beta period is over */
 			hq_downIcon.append( hq_dropArrowDownSVG );
 			hq_dropArrow.after( hq_dropArrowUpSVG, hq_dropArrowDownSVG );
 			hq_clubXXX.append( hq_clubXXXSVG );
+			hq_MemPosition.prepend( hq_MemPositionSVG );
 			hq_clubPublic.append( hq_clubPublicSVG );
 			hq_clubHidden.append( hq_clubHiddenSVG );
 			hq_clubHeader.prepend( hq_clubHeaderSVG );
@@ -741,6 +744,7 @@ onReady(function() {/* TODO Remove #whole when beta period is over */
 			hq_BuyGoldToName.appendTo( hq_BuyGoldToFlower ), hq_BuyGoldCleanUp.remove(),// NOTE Merge the two buy gold links to one
 			hq_HomePagePresentation.addClass( hq_ClassHpPresentation ),
 			hq_HomePageLookingFor.addClass( hq_ClassHpLookingFor ),
+			hq_MemberPosition.parent().addClass( hq_ClassMemPosition ),
 			hq_HomePageFlex.not( ( hq_NoHomePageList ) ).addClass( hq_ClassFlexContainer ),
 			hq_MakeFlexItem.addClass( hq_ClassFlexItem ).removeClass('odd even');
 		}
